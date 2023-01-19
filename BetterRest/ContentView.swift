47249a08b8e4771eb3a91a1c6a3ca3d9322f7b92
a-bonzo <alikhorramipour@gmail.com>
2023-01-19 14:05:08 +0330

    Initial commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,128 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Paul Hudson on 22/10/2021.
+//
+
+import CoreML
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 0
+
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section{
+                    VStack (alignment: .leading, spacing:7){
+                        Text("When do you want to wake up?")
+                            .font(.headline)
+
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
+                }
+
+                Section{
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+
+                Section{
+                    //Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker("Daily amount of coffee", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            Text($0 == 1 ? "1 cup" : "\($0) cups")
+                        }
+                    }.font(.headline)
+                }
+                
+                Section{
+                    Text("\(calculateBedtimeNoButton())")
+                        .font(.headline)
+                    
+                } header: {
+                    Text("Your ideal bedtime")
+                }
+            }
+            .navigationTitle("BetterRest")
+//            .toolbar {
+//                Button("Calculate", action: calculateBedtime)
+//            }
+//            .alert(alertTitle, isPresented: $showingAlert) {
+//                Button("Bruh") { }
+//            } message: {
+//                Text(alertMessage)
+//            }
+        }
+    }
+    
+    func calculateBedtimeNoButton() -> String{
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        let result: String
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            result = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your ideal bedtime"
+            result = "bruh!"
+        }
+        return result
+    }
+
+    func calculateBedtime(){
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your ideal bedtime"
+        }
+
+//        showingAlert = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# BetterRest
+ Sleep duration prediction using machine learning based on the cups of coffee you've drank!  
+ ![BetterRest](BetterRest.gif)
+# Installation
+ 1. Open up Xcode
+ 2. Click on File > Open
+ 3. Select BetterRest.xcodeproj
+ 4. Press Command+R to Build and Run
+# Training the Model
+ There is a trained model included named SleepCalculator.mlmodel but in case you want to train your own model using different techniques:
+ 1. Open up Xcode
+ 2. Click on Xcode > Open Developer Tool > Create ML
+ 3. Press New Document
+ 4. Select Tabular Regression
+ 5. Select BetterRest.csv as the training data
+ 6. Choose estimatedSleep as the Target
+ 7. Choose the features (recommended: all features) and the algorithm (recommended: Automatic) you want to run
+ 8. Press Train
+ 9. From Output press Get to save the model
+ 10. Add it to your project
+ 
